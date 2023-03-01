78b7976a24d5f66edc5eb951cfac5ffb2ceee581
Colin Ciervo <colinciervo@@gmail.com>
2023-03-01 13:26:50 -0500

    Add homepage/card page

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import fetchAll from '../apiCalls';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetchAll()
+      .then(data => {
+        console.log(data);
+        setCards(data)
+      })
+  }, [])
+
+  const showCards = () => {
+    return cards.map(card => {
+      return <p>{card.name}</p>
+    })
+  }
+
   return (
     <div>
-      
+      {showCards()}
     </div>
   );
 }
