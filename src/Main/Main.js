b71781f9e9b17b8259ef615f2dfec2c72d2e538e
Colin Ciervo <colinciervo@@gmail.com>
2023-03-03 00:40:36 -0500

    add error handling

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,6 +7,8 @@ import Card from '../Card/Card';
 const Main = () => {
     const [cards, setCards] = useState([])
     const [number, setNumber] = useState(1)
+    const [input, setInput] = useState('')
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         fetchAll(number)
@@ -30,24 +32,55 @@ const Main = () => {
     }
 
     const nextPage = () => {
-        setNumber(number + 1)
+        if (number < 787) {
+            setNumber(number + 1)
+            setError(false)
+        }
     }
 
     const prevPage = () => {
-        setNumber(number - 1)
+        if (number > 1) {
+            setNumber(number - 1);
+            setError(false)
+        }
     }
 
+    const holdInput = (event) => {
+        setInput(event.target.value)
+    }
+
+    const jumpTo = (event) => {
+        event.preventDefault()
+        if (input >= 1 && input <= 787) {
+            setNumber(input)
+            setInput('')
+            setError(false)
+        } else {
+            setError(true)
+        }
+    }
 
     return (
-        <div className='cards'>
-            {showCards()}
-            <div>
-                <p>Page Number: {number}</p>
-                <button onClick={prevPage}>Previous Page</button>
-                <button onClick={nextPage}>Next Page</button>
+        <div>
+            <form>
+                <label htmlFor='page'>Jump to Page: </label>
+                <input id='page' name='page' onChange={holdInput} value={input} />
+                <button onClick={jumpTo}>Go</button>
+            </form>
+            {error && (
+                <p>Please enter a valid page number between 1 and 787.</p>
+            )}
+            <div className='cards'>
+                {showCards()}
+                <div>
+                    <p>Page Number: {number}</p>
+                    <button onClick={prevPage}>Previous Page</button>
+                    <button onClick={nextPage}>Next Page</button>
+                </div>
             </div>
         </div>
     );
 }
 
-export default Main
+export default Main;
+
