fef6c38b55c49f3e8762aba2eb37690c85805cbd
Colin Ciervo <colinciervo@@gmail.com>
2023-03-05 15:08:56 -0500

    Cleaned code

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,18 +4,18 @@ import './Card.css'
 const Card = ({ img, id, name }) => {
     return (
         img ?
-            <Link to={`/details/${id}`} state={{ id: id}}>
+            <Link to={`/details/${id}`} state={{ id: id }}>
                 <img className='reg-card' src={img} id={id} alt={name} />
             </Link>
             :
             <div className='no-image'>
                 <h2 id={id}>No Image Available For {name}</h2>
-                <Link to={`/details/${id}`} state={{ id: id}}>
+                <Link to={`/details/${id}`} state={{ id: id }}>
                     <p className='no-img-link'>Click For More</p>
                 </Link>
-            </div> 
+            </div>
     );
-       
+
 };
 
 export default Card
