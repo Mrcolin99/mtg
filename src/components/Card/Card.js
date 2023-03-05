fff99a285ff892423e7c79d24c12add81e208cbb
Colin Ciervo <colinciervo@@gmail.com>
2023-03-05 15:06:05 -0500

    Add some styling to cards without images

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom"
+import './Card.css'
 
 const Card = ({ img, id, name }) => {
     return (
         img ?
             <Link to={`/details/${id}`} state={{ id: id}}>
-                <img src={img} id={id} alt={name} />
+                <img className='reg-card' src={img} id={id} alt={name} />
             </Link>
             :
-            <div>
+            <div className='no-image'>
                 <h2 id={id}>No Image Available For {name}</h2>
                 <Link to={`/details/${id}`} state={{ id: id}}>
-                    <p>Click For More</p>
+                    <p className='no-img-link'>Click For More</p>
                 </Link>
             </div> 
     );
